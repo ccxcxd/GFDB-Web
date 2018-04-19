b1c323c56a1f1a18a240c7e55ba805f2c6732ebf
Xi Chen <xx.chen@live.com>
2018-04-19 14:00:11 -0700

    make translation backend + autodetect

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
 ﻿$(document).ready(function () {
-    i18next.init({
-        lng: "zh_CN",
-        resources: i18n_resources
-    }, function (err, t) {
-        jqueryI18next.init(i18next, $);
-        $("[data-i18n]").localize();
-    });
-    
-    document.title = $.t("title");
+    i18next
+        .use(i18nextBrowserLanguageDetector)
+        .use(i18nextXHRBackend)
+        .init({
+            backend: {
+                loadPath: "locales/{{lng}}/{{ns}}.json",
+                parse: (data) => JSON.parse(data)
+            },
+            fallbackLng: "zh-CN"
+        }, function (err, t) {
+            jqueryI18next.init(i18next, $);
+            $("[data-i18n]").localize();
+            document.title = $.t("title");
+            setup_page();
+        });
+});
 
+function setup_page() {
     var map_tbl_sort = new Tablesort(document.getElementById("map_table"));
     var team_tbl_sort = new Tablesort(document.getElementById("team_table"));
-    
+
     var mission_info, spot_info, enemy_team_info, enemy_in_team_info;
     var enemy_character_type_info, ally_team_info, campaign_info;
     $.when(
-        $.getJSON("jsons/mission_info.json", function(data) {
+        $.getJSON("jsons/mission_info.json", function (data) {
             mission_info = data;
         }),
-        $.getJSON("jsons/spot_info.json", function(data) {
+        $.getJSON("jsons/spot_info.json", function (data) {
             spot_info = data;
         }),
-        $.getJSON("jsons/enemy_team_info.json", function(data) {
+        $.getJSON("jsons/enemy_team_info.json", function (data) {
             enemy_team_info = data;
         }),
-        $.getJSON("jsons/enemy_in_team_info.json", function(data) {
+        $.getJSON("jsons/enemy_in_team_info.json", function (data) {
             enemy_in_team_info = data;
         }),
-        $.getJSON("jsons/enemy_character_type_info.json", function(data) {
+        $.getJSON("jsons/enemy_character_type_info.json", function (data) {
             enemy_character_type_info = data;
         }),
-        $.getJSON("jsons/ally_team_info.json", function(data) {
+        $.getJSON("jsons/ally_team_info.json", function (data) {
             ally_team_info = data;
         }),
         $.getJSON("jsons/campaign_info.json", function (data) {
@@ -78,13 +86,12 @@
             });
             $("#map_select").change();
         });
-        
+
         $("#map_select").change(function () {
             $("#map_table tbody").empty();
             var mission_id = Number($("#map_select option:checked").val());
             $.each(spot_info, function (id, spot) {
-                if (spot.mission_id === mission_id)
-                {
+                if (spot.mission_id === mission_id) {
                     var enemy_team_id;
                     if (spot.enemy_team_id !== 0)
                         enemy_team_id = spot.enemy_team_id;
@@ -92,7 +99,7 @@
                         enemy_team_id = ally_team_info[spot.ally_team_id].enemy_team_id;
                     else
                         return true;
-                    
+
                     var enemy_team = enemy_team_info[enemy_team_id];
                     var count_dict = {};
                     $.each(enemy_team.member_ids, function (index, member_id) {
@@ -103,7 +110,7 @@
                     $.each(count_dict, function (key, value) {
                         count += key + "*" + value + " ";
                     });
-                    
+
                     $("<tr>").append(
                         $("<td>").text(enemy_team_id).attr("data-team_id", ""),
                         $("<td>").text(enemy_character_type_info[enemy_team.enemy_leader].name),
@@ -151,5 +158,4 @@
 
         $("#campaign_select").change();
     });
-
-});
+}

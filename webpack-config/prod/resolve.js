66f6a975dda292b7bca58cc14ff0b05517120b47
runtu <huorunquan@126.com>
2019-04-10 19:27:51 +0800

    IMP: 移除旧位置的配置文件

--- webpack-config/prod/resolve.js
+++
@@ -1,7 +0,0 @@
-const baseReso = require('../public/resolve')
-
-module.exports = (conf) => {
-  const baseResolve = baseReso(conf)
-  baseResolve.alias['jquery'] = 'jquery/dist/jquery.min.js'
-  return baseResolve
-}

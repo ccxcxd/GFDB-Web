fdb15b0a71e4a54f5c281358ca7f9527f7fe8178
runtu <huorunquan@126.com>
2019-04-11 11:39:48 +0800

    IMP: 完善 babel 配置

--- babel.config.js
+++ babel.config.js
@@ -18,7 +18,11 @@ module.exports = function (api) {
   ];
   const plugins = [
     '@babel/plugin-proposal-class-properties',
-    ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }],
+    ['import', {
+      'libraryName': 'antd',
+      // 'libraryDirectory': 'es',
+      'style': 'css',
+    }],
     ['lodash'],
   ];
 
